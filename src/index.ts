04ebfc0cb24d8a79e954677b30db080e347ca0cd
Aarav Shukla <aarav.shukla105@gmail.com>
2025-05-15 21:07:15 +0530

    Added gitignore file

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
 import express from "express";
 import mongoose from 'mongoose';
 import jwwt from "jsonwebtoken";
+import { UserModel } from "./db";
 
 const app = express();
+app.use(express.json());
 
-app.post("api/v1/signin", (req, res) => {
+app.post("api/v1/signin", async (req, res) => {
+    //zod validation
+    const username = req.body.username;
+    const password = req.body.password;
 
+    await UserModel.create({
+        username: username,
+        password: password 
+    })
+
+    res.json({
+        message: "User signed in successfully"
+    })
 })
 
 app.post("api/v1/content", (req, res) => {
